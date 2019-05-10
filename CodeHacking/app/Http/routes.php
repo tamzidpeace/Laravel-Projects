1cb4c5359fedf5e0143e14dc0cdb98db9b20289c
tamzidpeace <tamjedpeace@gmail.com>
2019-05-10 23:01:34 +0600

    showing users

--- CodeHacking/app/Http/routes.php
+++ CodeHacking/app/Http/routes.php
@@ -20,3 +20,7 @@ Route::auth();
 Route::get('/home', 'HomeController@index');
 
 Route::resource('admin/users', 'AdminUsersController');
+
+ Route::get('/admin', function () {
+     return view('admin.index');
+});
---
+++ CodeHacking/resources/views/admin/users/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Users</h1>
+
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Created</th>
+            <th scope="col">Updated</th>
+        </tr>
+        </thead>
+        <tbody>
+        @if ($users)
+
+            @foreach($users as $user)
+                <tr>
+                    <td>{{$user->id}}</td>
+                    <td>{{$user->name}}</td>
+                    <td>{{$user->email}}</td>
+                    <td>{{$user->created_at->diffForHumans()}}</td>
+                    <td>{{$user->updated_at->diffForHumans()}}</td>
+                </tr>
+            @endforeach
+
+        @endif
+        </tbody>
+    </table>
+
+@stop
