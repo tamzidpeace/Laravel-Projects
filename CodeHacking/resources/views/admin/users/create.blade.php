9592b606ecbb8406eff468dec890a671ea0b7615
tamzidpeace <tamjedpeace@gmail.com>
2019-05-11 20:50:17 +0600

    create form using laravel collective package

---
+++ CodeHacking/resources/views/admin/users/create.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.admin')
+
+@section('content')
+
+    <h1>created</h1>
+
+    {!! Form::open(['method' => 'POST', 'action' => 'AdminUsersController@store']) !!}
+
+    <div class="form-group">
+        {!! Form::label('name', 'Name') !!}
+        {!! Form::text('name', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!!  Form::submit('Create User', ['class' => 'btn btn-primary']) !!}
+    </div>
+
+    {!! Form::close() !!}
+    
+@stop
--- CodeHacking/resources/views/admin/users/index.blade.php
+++ CodeHacking/resources/views/admin/users/index.blade.php
@@ -9,6 +9,8 @@
             <th scope="col">ID</th>
             <th scope="col">Name</th>
             <th scope="col">Email</th>
+            <th scope="col">Role</th>
+            <th scope="col">Active</th>
             <th scope="col">Created</th>
             <th scope="col">Updated</th>
         </tr>
@@ -21,6 +23,8 @@
                     <td>{{$user->id}}</td>
                     <td>{{$user->name}}</td>
                     <td>{{$user->email}}</td>
+                    <td>{{$user->role->name}}</td>
+                    <td>{{$user->is_active == 1 ? 'Active' : 'Inactive'}}</td>
                     <td>{{$user->created_at->diffForHumans()}}</td>
                     <td>{{$user->updated_at->diffForHumans()}}</td>
                 </tr>
