3fce985f2834dc795f91859583e73cc01ac13423
tamzidpeace <tamjedpeace@gmail.com>
2019-05-23 23:30:45 +0600

    new project photoshow

---
+++ photoshow/app/Http/Controllers/AlbumsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AlbumsController extends Controller
+{
+    public function index() {
+        return view('albums.index');
+    }
+
+    public function create() {
+        return view('albums.create');
+    }
+}
