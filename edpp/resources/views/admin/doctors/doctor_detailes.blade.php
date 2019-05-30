2db66cdfaecf6c8117d4bd5c20c83788d5078bfc
tamzidpeace <tamjedpeace@gmail.com>
2019-05-30 21:19:31 +0600

    doctors and hospitals details page complete

--- edpp/resources/views/admin/doctors/doctor_detailes.blade.php
+++ edpp/resources/views/admin/doctors/doctor_detailes.blade.php
@@ -6,9 +6,29 @@
     <div class="row">
         <h1>Doctor Details</h1>
         <a style="margin-top:15px" class="btn btn-info" href="/admin/doctors">Go Back</a>
-
-
     </div>
-</div>
+
+    <div style="margin-top:30px;" class="row">
+        <div class="col-sm-6 panel-group">
+
+            <div class="panel panel-success">
+
+                <div class="panel-heading">
+                    <img class="img-thumbnail" style="height:120px; width:120px;" src="/images/doctor_image/{{$doctor->photo}}" alt="">
+                </div>
+
+                <div class="panel-body">
+                    <h4>Name: {{$doctor->name}} </h4>
+                    <h4>Email: {{$doctor->email}} </h4>
+                    <h4>Phone: {{$doctor->phone}}</h4>
+                    <h4>Address: {{$doctor->address}} </h4>
+                    <h4>Status: {{$doctor->status}} </h4>
+                </div>
+            </div>
+
+        </div>
+    </div>
     
+</div>
+
 @endsection
--- edpp/resources/views/admin/hospitals/hospital_details.blade.php
+++ edpp/resources/views/admin/hospitals/hospital_details.blade.php
@@ -6,9 +6,30 @@
     <div class="row">
         <h1>Hospital Details</h1>
         <a style="margin-top:15px" class="btn btn-info" href="/admin/hospitals">Go Back</a>
-
-
     </div>
+
+    <div style="margin-top:30px;" class="row">
+        <div class="col-sm-6 panel-group">
+
+            <div class="panel panel-success">
+
+                <div class="panel-heading">
+                    <img class="img-thumbnail" style="height:120px; width:120px;"
+                        src="/images/hospital_image/{{$hospital->photo}}" alt="">
+                </div>
+
+                <div class="panel-body">
+                    <h4>Name: {{$hospital->name}} </h4>
+                    <h4>Email: {{$hospital->email}} </h4>
+                    {{-- <h4>Phone: {{$doctor->phone}}</h4> --}}
+                    <h4>Address: {{$hospital->address}} </h4>
+                    <h4>Status: {{$hospital->status}} </h4>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
 </div>
 
 @endsection
