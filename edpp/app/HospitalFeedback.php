af139812b9c7d497cd8122480892958a5c814b5f
tamzidpeace <tamjedpeace@gmail.com>
2019-08-20 07:27:38 +0600

    hospital feedback 2

---
+++ edpp/app/HospitalFeedback.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HospitalFeedback extends Model
+{
+    //
+    protected $fillable = [
+        'feedback',
+    ];
+}
--- edpp/app/Http/Controllers/FeedBackController.php
+++ edpp/app/Http/Controllers/FeedBackController.php
@@ -4,11 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\HospitalFeedback;
 
 class FeedBackController extends Controller
 {
     //
-    public function hospitalFeedback() {
-        return 123;
+    public function hospitalFeedback(Request $request) {
+        
+        $this->validate($request, [
+            'feedback' => 'required',
+        ]);
+
+        $hospitalFeedback = new HospitalFeedback;
+        
+        $user = Auth::user();
+        $hospitalFeedback->name = $user->name;
+        $hospitalFeedback->email = $user->email;
+        $hospitalFeedback->feedback = $request->feedback;
+
+        $hospitalFeedback->save();
+        
+        return back()->with('success', 'Thanks for your feedback.');
     }
 }
